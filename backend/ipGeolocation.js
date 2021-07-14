23af4f1ffd1be68d66a1d7971fa7a1856c2e2a12
Konstantinos Tsakonas <iamtsac@gmail.com>
2021-07-14 15:17:11 +0300

    preparing heatmap data

--- backend/ipGeolocation.js
+++ backend/ipGeolocation.js
@@ -1,17 +1,17 @@
 module.exports.ipGeolocation =  function ipGeolocation(app, connection) {
     app.get('/ipInfo', function (request, response) {
         var name = request.headers;
-        connection.query('SELECT DISTINCT serverIPAddress as ip  FROM Entry where username=(?)', [name['username']], function(err,result,fields){
+        connection.query('SELECT DISTINCT serverIPAddress as ip, count(*) as numReq FROM Entry where username=(?) AND serverIPAddress IS NOT NULL group by serverIPAddress;', [name['username']], function(err,result,fields){
             if (err) throw  err;
-            var ips = []; 
+            var ips = new Object(); 
             for(var i in result){
-                ips[i] = result[0]['ip']
+                if(result[i]['ip'] !== '')
+                    ips[result[i]['ip'].match(/\w.*\w/)[0].toString()]=result[i]['numReq']
             }
+            console.log(ips)
             response.send(ips)
             });
-
-            
-            
-        //response.sendFile(path.resolve('html/profileSettings.html'));
     });
+
+        //response.sendFile(path.resolve('html/profileSettings.html'));
 }
--- html/scripts/ipHandle.js
+++ html/scripts/ipHandle.js
@@ -1,7 +1,6 @@
 var but = document.getElementById('ip');
 function handleIP(){
     var cookies = JSON.parse(document.cookie);
-
     fetch("/ipInfo",
         {
             method: 'GET',
@@ -10,18 +9,25 @@ function handleIP(){
         .then(res => res.json())
         .then(data => {
             var ips = [];
-            var endpoint = "http://ip-api.com/batch?fields=lat,lon,status,isp"
+            var endpoint = "http://ip-api.com/batch?fields=query,lat,lon,status,isp"
+            var j = 0;
             for(var i in data){
-                ips[i] = data[i].match(/\w.*\w/)[0].toString();
+                if (i !== '')
+                    ips[j] = i
+                j++
             }
+            console.log(ips)
             fetch(endpoint,
                 {
                     method: 'POST',
                     body: JSON.stringify(ips)
                 })
                 .then(response => ( response.json()))
-                .then(data => {
-                    console.log(data)
+                .then(geo => {
+                    for(var i in geo){
+                        geo[i]['visits'] = data[geo[i]['query']]
+                    }
+                    console.log(geo)
                 })
 
         })
