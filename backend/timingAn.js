61e85001f988cb82e24db93765923bd5ed105ad1
Konstantinos Tsakonas <iamtsac@gmail.com>
2021-08-05 01:45:44 +0300

    data done, charts left

--- backend/timingAn.js
+++ backend/timingAn.js
@@ -1,14 +1,43 @@
 module.exports.timingAn =  function timingAn(app, connection) {
     app.post('/timingData', function (request, response) {
-        var group = request.body
-        console.log(request)
+        var filter = request.body;
+        let filterMap = {
+            'method':'method',
+            '\`content-typeResponse\`': 'type',
+            'dayname(startedDateTime)' : 'day'
+        }
 
-         connection.query('select avg(wait) as wait, `content-typeResponse` as type, method, startedDateTime as time from Entry group by (?)',[[group]], function(err,result,fields){
+         connection.query(`select avg(wait) as wait, date_format(startedDateTime,'%H') as hour, \`content-typeResponse\` as type, method, dayname(startedDateTime) as day from Entry group by date_format(startedDateTime,'%H'), ${filter['filter']}`, function(err,result,fields){
              if (err) throw  err;
-             var data = {};
+             var data = {}
+             var label = [];
              for( var i in result)
              {
+                 let key = result[i]['hour']
+                 let obj = {
+                     'wait': result[i].wait,
+                     'check' : result[i][filterMap[filter['filter']]]
+                 }
+                 label.push(result[i][filterMap[filter['filter']]])
+                 if( Object.keys(data).includes(key))
+                    data[key].push(obj)
+                 else
+                 { 
+                    data[key] = new Array()
+                    data[key].push(obj)
+                 }
              }
+             var arr = {}
+             var labels = new Set(label)
+             
+             arr = {
+                 'labels': labels.keys(),
+                 ...data
+             }
+             console.log(arr)
+             response.send(arr)
+
             });
     });
 }
+
--- html/scripts/timingAn.js
+++ html/scripts/timingAn.js
@@ -1,18 +1,67 @@
 var timingBut = document.getElementById('timing');
+var timingDiv = document.getElementById('timingDiv');
+timingDiv.style.display = 'none';
+var listEl = document.getElementById('filter')
 
-async function getData()
+function clickTimingBut()
 {
-    fetch('/timingData',{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'text/plain'
-        },
-        body: 
-    })
-    .then(res => res.text())
+    if (timingDiv.style.display === 'none')
+    {
+        timingDiv.style.display = 'block';
+        selectChange();
+    }
+    else
+        timingDiv.style.display = 'none';
+}
+
+async function selectChange()
+{
+
+    var filterType = listEl.options[listEl.selectedIndex].value;
+    let data = await getData(filterType)
+    console.log(data)
+    labels = data[1]
+    console.log(labels)
+    data = data[0]
+    let filterMap = {
+        'method':'method',
+        '\`content-typeResponse\`': 'type',
+        'dayname(startedDateTime)' : 'time'
+    }
+    for(var i in data)
+    {
+        for(var j in data[i])
+        {
+            var checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.value = data[i][j].check;
+            checkbox.id = data[i][j].check;
+            checkbox.name = data[i][j].check;
+            var label = document.createElement('label')
+            label.htmlFor =  data[i][j].check;
+            label.appendChild(document.createTextNode( data[i][j].check));
+ 
+            timingDiv.append(checkbox)
+            timingDiv.append(label)
+            console.log(data[i][j].check)
+        }
+    }
 
 }
 
+async function getData(filter)
+{
+    let b = { 'filter': filter}
+    let response = await fetch('/timingData',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(b)
+    });
+    return await response.json()
 
-var x = getData()
-console.log(x)
+}
+
+timingBut.onclick = clickTimingBut
+listEl.onchange = selectChange
