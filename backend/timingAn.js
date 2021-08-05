8ad6fb1466d8088782a23bbd4191d5ef0a1226ad
Konstantinos Tsakonas <iamtsac@gmail.com>
2021-08-05 22:58:40 +0300

    admin b done

--- backend/timingAn.js
+++ backend/timingAn.js
@@ -4,37 +4,35 @@ module.exports.timingAn =  function timingAn(app, connection) {
         let filterMap = {
             'method':'method',
             '\`content-typeResponse\`': 'type',
-            'dayname(startedDateTime)' : 'day'
+            'dayname(startedDateTime)' : 'day',
+            'isp' : 'isp'
         }
 
-         connection.query(`select avg(wait) as wait, date_format(startedDateTime,'%H') as hour, \`content-typeResponse\` as type, method, dayname(startedDateTime) as day from Entry group by date_format(startedDateTime,'%H'), ${filter['filter']}`, function(err,result,fields){
+         connection.query(`select avg(wait) as wait, date_format(startedDateTime,'%H') as hour, \`content-typeResponse\` as type, method,isp,  dayname(startedDateTime) as day from Entry  left join Ip_info on Entry.username = Ip_info.username group by date_format(startedDateTime,'%H'), ${filter['filter']}`, function(err,result,fields){
              if (err) throw  err;
              var data = {}
              var label = [];
-             for( var i in result)
+             for(var i in result)
              {
-                 let key = result[i]['hour']
-                 let obj = {
-                     'wait': result[i].wait,
-                     'check' : result[i][filterMap[filter['filter']]]
-                 }
-                 label.push(result[i][filterMap[filter['filter']]])
-                 if( Object.keys(data).includes(key))
-                    data[key].push(obj)
-                 else
-                 { 
-                    data[key] = new Array()
-                    data[key].push(obj)
-                 }
+                var padded = [];
+                let key = result[i][filterMap[filter['filter']]]
+                for( var j =0; j<24; j++)
+                {
+                        padded[j]=0;
+                }
+                padded[result[i]['hour']] = result[i].wait
+
+                if( Object.keys(data).includes(key))
+                    {
+                    data[key][result[i]['hour']] = result[i]['wait']
+                    }
+                else
+                { 
+                   data[key] = padded;
+                }
              }
-             
-             var toSend = {
-                 'labels': label,
-                 'data' : data
-             }
-             response.send(toSend)
+             response.send(data)
 
             });
     });
 }
-
--- html/scripts/timingAn.js
+++ html/scripts/timingAn.js
@@ -1,49 +1,42 @@
 var timingBut = document.getElementById('timing');
 var timingDiv = document.getElementById('timingDiv');
+var checkedList = [];
 timingDiv.style.display = 'none';
 var listEl = document.getElementById('filter')
+var checkP = document.createElement('p')
+let chartPlaceHolder = document.createElement('canvas')
+timingDiv.append(chartPlaceHolder)
+timingDiv.append(checkP)
 
-function clickTimingBut()
-{
-    if (timingDiv.style.display === 'none')
-    {
-        timingDiv.style.display = 'block';
-        selectChange();
-    }
-    else
-        timingDiv.style.display = 'none';
+let chartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart'
+      }
+        }
+            
 }
 
-async function selectChange()
-{
+let charts = new Chart(chartPlaceHolder, {
+                type:'line',
+                data:{
+                    labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
+                    datasets: [],
+                    borderColor:'#ff0000'
+                },
+                options: chartOptions,
+                title: {
+                  display: true,
+                  text: 'Chart.js Line Chart'
+          
+                }
+            });
 
-    var filterType = listEl.options[listEl.selectedIndex].value;
-    let data = await getData(filterType)
-    var labels = [...new Set(data['labels'])]
-    data = data['data']
-
-    for(var i in labels)
-    {
-            var checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
-            checkbox.value = labels[i];
-            checkbox.id =    'check';
-            checkbox.name =  labels[i];
-            var label = document.createElement('label')
-            label.htmlFor =  labels[i];
-            label.appendChild(document.createTextNode(labels[i]));
-            timingDiv.append(checkbox);
-            timingDiv.append(label);
-
-    }
-    let filterMap = {
-        'method':'method',
-        '\`content-typeResponse\`': 'type',
-        'dayname(startedDateTime)' : 'time'
-    }
-    createChart(data)
-
-}
 
 async function getData(filter)
 {
@@ -59,10 +52,134 @@ async function getData(filter)
 
 }
 
-function createChart(dataObj)
+async function clickTimingBut()
 {
-    console.log(dataObj)
+    if (timingDiv.style.display === 'none')
+    {
+        timingDiv.style.display = 'block';
+        selectChange();
+    }
+    else
+        timingDiv.style.display = 'none';
+    var filterType = listEl.options[listEl.selectedIndex].value;
+    let data = await getData(filterType)
+    createChart(data)
 }
 
+async function selectChange()
+{
+
+    var filterType = listEl.options[listEl.selectedIndex].value;
+    let data = await getData(filterType)
+    checkP.innerHTML = ' '
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.value = 'ALL';
+    checkbox.id = 'all';
+    checkbox.name =  'ALL';
+    checkbox.onclick = checkAll;
+    checkbox.classList.add("checkbox");
+    var label = document.createElement('label')
+    label.htmlFor =  'ALL';
+    label.appendChild(document.createTextNode('ALL'));
+    checkP.append(checkbox);
+    checkP.append(label);
+    document.getElementById('all').checked = true;
+
+    for(var i in data)
+    {
+            var checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.value = i;
+            checkbox.id = i;
+            checkbox.name =  i;
+            checkbox.onclick =  checkBoxes;
+            checkbox.classList.add("checkbox");
+            var label = document.createElement('label')
+            label.htmlFor =  i;
+            label.appendChild(document.createTextNode(i));
+            checkP.append(checkbox);
+            checkP.append(label);
+
+    }
+    createChart(data)
+}
+
+
+
+async function checkBoxes()
+{
+   var filterType = listEl.options[listEl.selectedIndex].value;
+   let data = await getData(filterType)
+   var box = document.getElementById('all')
+   box.checked = false;
+   checkedList=[];
+   var elements = document.getElementsByClassName("checkbox");
+   for (var i = 0, len = elements.length; i < len; i++) 
+   {
+                if((elements[i].checked === true))
+                checkedList.push(elements[i].id)
+                
+    }
+    if (checkedList.length === 0)
+        box.checked = true
+    console.log(checkedList)
+    createChart(data)
+}
+
+
+
+function createChart(dataObj)
+{
+    charts.data.datasets=[]
+    charts.update()
+    var box = document.getElementById('all')
+    if(box.checked === false)
+    {
+        for(var i in checkedList)
+        {
+           newDataset = {
+                label: checkedList[i],
+                data: dataObj[checkedList[i]]
+               }
+                charts.data.datasets.push(newDataset);
+                charts.update()
+            }
+    }
+    else
+    {
+
+        for(var i in dataObj)
+        {
+           newDataset = {
+                label: i,
+                data: dataObj[i]
+               }
+                charts.data.datasets.push(newDataset);
+                charts.update()
+        }
+    }
+}
+    
+async function checkAll()
+{
+   var filterType = listEl.options[listEl.selectedIndex].value;
+   let data = await getData(filterType)
+   var elements = document.getElementsByClassName("checkbox");
+   var box = document.getElementById('all')
+   if(box.checked === true)
+    {
+        for (var i = 0, len = elements.length; i < len; i++) 
+        {
+                     elements[i].checked =false;
+                     
+        }
+    }
+    box.checked = true;
+    createChart(data)
+}
+
+
 timingBut.onclick = clickTimingBut
+//listEl.onchange = selectChange
 listEl.onchange = selectChange
